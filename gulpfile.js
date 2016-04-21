96a8720516ae3bb0602f82ad1745bb84133300a8
Charles Waddell <charles@silverorange.com>
2016-04-21 11:37:09 -0300

    Switch between local and composer packages task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 
-var symlinks = require('./lib/symlinks');
+var vendor_symlinks = require('./lib/vendor-symlinks');
+var package_symlinks = require('./lib/package-symlinks');
 var concentrate = require('./lib/concentrate');
 var paths = require('./lib/paths');
 var flags = require('./lib/flags');
@@ -14,8 +15,11 @@ var less = require('./lib/less');
 
 gulp.task('phpclassmap', ['setup-symlinks'], phpclassmap.task);
 gulp.task('phplint', ['setup-symlinks'], phplint.task);
-gulp.task('setup-symlinks', symlinks.task.setup);
-gulp.task('teardown-symlinks', symlinks.task.teardown);
+gulp.task('setup-vendor-symlinks', vendor_symlinks.task.setup);
+gulp.task('teardown-vendor-symlinks', vendor_symlinks.task.teardown);
+gulp.task('setup-package-symlinks', package_symlinks.task.setup);
+gulp.task('teardown-package-symlinks', package_symlinks.task.teardown);
+gulp.task('setup-symlinks', [ 'setup-package-symlinks', 'setup-vendor-symlinks' ]);
 gulp.task('clean', ['teardown-symlinks'], clean.task);
 gulp.task('concentrate-internal', ['setup-symlinks'], concentrate.task);
 gulp.task('concentrate', ['concentrate-internal'], function() {
@@ -28,7 +32,8 @@ function cleanShutdown() {
   gutil.log(gutil.colors.blue('BYE'));
 
   flags.remove();
-  symlinks.task.teardown();
+  vendor_symlinks.task.teardown();
+  package_symlinks.task.teardown();
 
   process.exit();
 }
