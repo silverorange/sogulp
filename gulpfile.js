795cb378e2fdb99d072721696b795c0b2850fa64
Michael Gauthier <mike@silverorange.com>
2020-09-29 12:06:11 -0300

    Add prettier to project

--- gulpfile.js
+++ gulpfile.js
@@ -25,19 +25,70 @@ function symlinkSetupTask() {
 
 if (options.symlinks.length) {
   gulp.task('teardown-symlinks', symlinks.task.teardown);
-  gulp.task('setup-symlinks', gulp.series('teardown-symlinks', symlinkSetupTask));
-  gulp.task('phpclassmap', gulp.series('setup-symlinks', (done) => { phpclassmap.task(); done(); }));
-  gulp.task('phplint', gulp.series('setup-symlinks', (done) => { phplint.task(); done(); }));
-  gulp.task('clean', gulp.series('teardown-symlinks', (done) => { clean.task(); done(); }));
-  gulp.task('concentrate-internal', gulp.series('setup-symlinks', (done) => { concentrate.task(); done(); }));
-  gulp.task('build-less', gulp.series('setup-symlinks', (done) => { less.task(); done(); }));
-  gulp.task('concentrate', gulp.series('concentrate-internal', 'teardown-symlinks'));
+  gulp.task(
+    'setup-symlinks',
+    gulp.series('teardown-symlinks', symlinkSetupTask)
+  );
+  gulp.task(
+    'phpclassmap',
+    gulp.series('setup-symlinks', (done) => {
+      phpclassmap.task();
+      done();
+    })
+  );
+  gulp.task(
+    'phplint',
+    gulp.series('setup-symlinks', (done) => {
+      phplint.task();
+      done();
+    })
+  );
+  gulp.task(
+    'clean',
+    gulp.series('teardown-symlinks', (done) => {
+      clean.task();
+      done();
+    })
+  );
+  gulp.task(
+    'concentrate-internal',
+    gulp.series('setup-symlinks', (done) => {
+      concentrate.task();
+      done();
+    })
+  );
+  gulp.task(
+    'build-less',
+    gulp.series('setup-symlinks', (done) => {
+      less.task();
+      done();
+    })
+  );
+  gulp.task(
+    'concentrate',
+    gulp.series('concentrate-internal', 'teardown-symlinks')
+  );
 } else {
-  gulp.task('phpclassmap', (done) => { phpclassmap.task(); done(); });
-  gulp.task('phplint', (done) => { phplint.task(); done(); });
-  gulp.task('clean', (done) => { clean.task(); done(); });
-  gulp.task('build-less', (done) => { less.task(); done(); });
-  gulp.task('concentrate', (done) => { concentrate.task(); done(); });
+  gulp.task('phpclassmap', (done) => {
+    phpclassmap.task();
+    done();
+  });
+  gulp.task('phplint', (done) => {
+    phplint.task();
+    done();
+  });
+  gulp.task('clean', (done) => {
+    clean.task();
+    done();
+  });
+  gulp.task('build-less', (done) => {
+    less.task();
+    done();
+  });
+  gulp.task('concentrate', (done) => {
+    concentrate.task();
+    done();
+  });
 }
 
 gulp.task('write-flag', gulp.series('build-less', flags.task));
@@ -58,16 +109,17 @@ function cleanShutdown() {
  * Watches LESS and JS files for changes and recompiles/minifies/bundles
  * them.
  */
-const dependencies = (options.symlinks.length) ?
-  gulp.series('setup-symlinks', 'write-flag') : 'write-flag';
+const dependencies = options.symlinks.length
+  ? gulp.series('setup-symlinks', 'write-flag')
+  : 'write-flag';
 
-gulp.task('default', gulp.series(dependencies, () => {
-  process.on('SIGINT', cleanShutdown);
-  process.on('SIGHUP', cleanShutdown);
-  process.on('SIGTERM', cleanShutdown);
+gulp.task(
+  'default',
+  gulp.series(dependencies, () => {
+    process.on('SIGINT', cleanShutdown);
+    process.on('SIGHUP', cleanShutdown);
+    process.on('SIGTERM', cleanShutdown);
 
-  return Promise.all([
-    phpwatcher.watch(),
-    lesswatcher.watch(),
-  ]);
-}));
+    return Promise.all([phpwatcher.watch(), lesswatcher.watch()]);
+  })
+);
